fbba1651a05749e4a959c62b4201b09877569943
khnzgfff <mohamed.2023creative@gmail.com>
2026-04-22 01:45:01 +0200

    Implement WebView with privacy features

---
+++ MainActivity.java
@@ -0,0 +1,86 @@
+package com.stealth.browser;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        
+        // --- إعدادات WebView الأساسية ---
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setUserAgentString("Mozilla/5.0 (Linux; Android 14; Pixel 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36");
+        
+        // تعطيل حفظ كلمات المرور والإكمال التلقائي
+        settings.setSavePassword(false);
+        settings.setSaveFormData(false);
+        
+        // --- إعدادات التخفي المتقدمة ---
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+                // 1. حقن كود تعطيل WebRTC
+                String disableWebRTC = "javascript:(function() {" +
+                        "window.RTCPeerConnection = null;" +
+                        "window.webkitRTCPeerConnection = null;" +
+                        "window.mozRTCPeerConnection = null;" +
+                        "if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {" +
+                        "    navigator.mediaDevices.getUserMedia = null;" +
+                        "}" +
+                        "})();";
+                view.loadUrl(disableWebRTC);
+
+                // 2. حقن كود تزوير Canvas Fingerprint
+                String spoofCanvas = "javascript:(function() {" +
+                        "const originalGetContext = HTMLCanvasElement.prototype.getContext;" +
+                        "HTMLCanvasElement.prototype.getContext = function(type, ...args) {" +
+                        "    const context = originalGetContext.call(this, type, ...args);" +
+                        "    if (type === '2d') {" +
+                        "        const originalFillText = context.fillText;" +
+                        "        context.fillText = function(text, x, y) {" +
+                        "            return originalFillText.call(this, text, x + 0.1, y + 0.1);" +
+                        "        };" +
+                        "    }" +
+                        "    return context;" +
+                        "};" +
+                        "})();";
+                view.loadUrl(spoofCanvas);
+
+                // 3. حقن كود إخفاء بصمة الخطوط
+                String hideFonts = "javascript:(function() {" +
+                        "Object.defineProperty(document, 'fonts', {" +
+                        "    get: function() { return undefined; }" +
+                        "});" +
+                        "})();";
+                view.loadUrl(hideFonts);
+                
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+
+        // تحميل الموقع الافتراضي (يمكن تغييره من config.json)
+        webView.loadUrl("https://freecash.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
